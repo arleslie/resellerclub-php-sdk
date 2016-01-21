655b457dff4e7f5416880816bbf203bfd805ced3
Andrew Leslie <arleslie@arleslie.com>
2016-01-21 00:33:49 -0500

    Adding support for XML calls and API calls that have an additional api prefix

--- src/Helper.php
+++ src/Helper.php
@@ -16,12 +16,22 @@ trait Helper
 		$this->guzzle = $guzzle;
 	}
 
-	protected function get($method, $args)
+	protected function get($method, $args, $prefix = '')
 	{
 		return $this->parse(
 			$this->guzzle->get(
-				$this->api .'/'. $method.'.json?'.str_replace(['%5B0','%5D'], '', http_build_query(array_merge($args, $this->creds)))
+				$this->api .'/'. $prefix . $method.'.json?'.str_replace(['%5B0','%5D'], '', http_build_query(array_merge($args, $this->creds)))
 			)
+		);
+	}
+
+	protected function getXML($method, $args, $prefix = '')
+	{
+		return $this->parse(
+			$this->guzzle->get(
+				$this->api .'/'. $prefix . $method.'.xml?'.str_replace(['%5B0','%5D'], '', http_build_query(array_merge($args, $this->creds)))
+			),
+			'xml'
 		);
 	}
 
@@ -32,8 +42,15 @@ trait Helper
 		);
 	}
 
-	protected function parse(Response $response)
+	protected function parse(Response $response, $type = 'json')
 	{
-		return json_decode((string) $response->getBody(), true);
+		switch ($type) {
+			case 'json':
+				return json_decode((string) $response->getBody(), true);
+			case 'xml':
+				return simplexml_load_file((string) $response->getBody());
+			default:
+				throw new Exception("Invalid repsonse type");
+		}
 	}
 }
